fbbe2348a32b01fd4296ef466fc034e1eb6f8260
quangtien1996 <dautien69@gmail.com>
2019-02-19 19:20:23 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+ 
+<html>
+ 
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title>Facebook login</title>
+    </head>
+ 
+    <body>
+ 
+        <a href="https://www.facebook.com/dialog/oauth?client_id=1437446853027250&redirect_uri=https://localhost/fblogin/callback.php&scope=public_profile"><img src="facebook-sign-in.png"/></a>
+ 
+    </body>
+ 
+</html>
